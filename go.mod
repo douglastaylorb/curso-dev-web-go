c5d3d281cab8126d114ea82ff65c1be09ff677c8
douglastaylorb <douglastaylor20@gmail.com>
2025-01-02 15:09:55 -0300

    Inicio modulo 3

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/douglastaylorb/curso-dev-web-go
+
+go 1.23.4
--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
+func noteList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set(("Content-Type"), "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Lista de Anotações</h1>")
+}
+
+func noteView(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "ID é obrigatório", http.StatusBadRequest)
+		return
+	}
+	note := `
+		<div>
+			<h3> Nota %s </h3>
+			<p> Detalhes da nota %s </p>
+		</div>
+	`
+
+	fmt.Fprint(w, note, id)
+}
+
+func noteCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprint(w, "Criando uma nova anotação")
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
-	http.ListenAndServe(":5000", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", noteList)
+	mux.HandleFunc("/note/view", noteView)
+	mux.HandleFunc("/note/create", noteCreate)
+
+	http.ListenAndServe(":5000", mux)
 
 }
